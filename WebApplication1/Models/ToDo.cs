4d0d927ff5ed5aed689fcc2dae5aa1b718eb6d79
namnc <namnche141707@fpt.edu.vn>
2022-10-17 10:10:35 +0700

    Deploy

---
+++ WebApplication1/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class ToDo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Priority { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ WebApplication1/Models/ToDoDemoContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication1.Models
+{
+    public partial class ToDoDemoContext : DbContext
+    {
+        public ToDoDemoContext()
+        {
+        }
+
+        public ToDoDemoContext(DbContextOptions<ToDoDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ToDo> ToDos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=ToDoDemo;Username=postgres;Password=canhnamakashi");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDo>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
